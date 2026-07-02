078e66ea2fea24cfec20e1e6d3afe039bdd9da9e
ONION\Public <e1c24013@oit.ac.jp>
2026-07-02 19:14:50 +0900

    feat：オンライン機能追加

---
+++ FridayGroup/Assets/Main/Script/System/GameManager.cs
@@ -0,0 +1,29 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private NetworkPrefabRef playerPrefab;
+    [SerializeField] private Transform spawnPoint;
+    // プレイヤーがルームに参加した時に呼ばれるコールバック等で処理します
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        // サーバー（またはホスト）だけがSpawnを実行する権限を持ちます
+        if (runner.IsServer)
+        {
+            // 第4引数の「player」が振り分けの鍵です。
+            // ここで「このオブジェクトの入力権限はこのプレイヤーに与える」と指定しています。
+            NetworkObject playerObject = runner.Spawn(
+                playerPrefab,
+                spawnPoint.position,
+                Quaternion.identity,
+                player // ← ここで入力権限（Input Authority）を割り当て！
+            );
+
+            Debug.Log($"プレイヤー {player.PlayerId} のオブジェクトを生成しました");
+        }
+    }
+}
