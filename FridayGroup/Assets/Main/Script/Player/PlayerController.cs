068644023638e0090d2df9cd2bd33bfd187f7130
Ukita <e1c24013@oit.ac.jp>
2026-07-10 18:09:24 +0900

    Revert "feat：オンライン機能"
    This reverts commit 57c1a9d6c7a716656be0d118c52878a0ca6917ae.

--- FridayGroup/Assets/Main/Script/Player/PlayerController.cs
+++ FridayGroup/Assets/Main/Script/Player/PlayerController.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
-using Fusion; // 必要に応じて追加
 
-// PlayerBase（NetworkBehaviour）を継承
+// 「: PlayerBase」と書くことで、PlayerBaseの機能を全て引き継ぐ（継承）
 public class PlayerController : PlayerBase
 {
     public int playerID; // このプレイヤー固有のID
 
-    // 【修正】override する対象を Start から Spawned に変更します
-    public override void Spawned()
+    // override: 親クラスのStart()を上書きしつつ、拡張する
+    protected override void Start()
     {
-        // base.Spawned() で親クラス(PlayerBase)のSpawned()を先に実行する
-        base.Spawned();
+        // base.Start() で親クラス(PlayerBase)のStart()を先に実行する
+        base.Start();
 
-        // --- ここから子クラス(PlayerController)独自の初期化処理 ---
+        // --- ここから子クラス(PlayerController)独自の処理 ---
 
-        // 例: プレイヤーIDの割り当てなど（必要に応じてコメントアウトを解除）
-        // playerID = 1;
-        // Debug.Log($"プレイヤー{playerID}が操作可能になりました！");
+        // プレイヤーIDの割り当てなどの初期設定
+        //playerID = 1;
+        //Debug.Log($"プレイヤー{playerID}が操作可能になりました！");
     }
+
+    // 親クラスで書いた Update や OnMove, OnActionA などの関数は、
+    // ここに書かなくても自動的に使えるようになっています！
 }
