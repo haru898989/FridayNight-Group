21c74ad79014b1ad1b55a5ef55d8177074cedb88
tsujimon <e1c24053@oit.ac.jp>
2026-07-18 21:26:24 +0900

    feat:スタート画面移行するためのボタンの追加

---
+++ FridayGroup/Assets/Main/Script/System/ResultManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultManager : MonoBehaviour
+{
+    public void BackToTitle()
+    {
+        SceneManager.LoadScene("OnlineConnect");
+    }
+}
